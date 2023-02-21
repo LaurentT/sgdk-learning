7c1f84119477e1d863243956392b6d34f94c4200
Laurent Tonon <ltonon@liveaction.com>
2023-02-21 11:18:59 +0100

    adding comments

--- src/main.c
+++ src/main.c
@@ -5,15 +5,24 @@ Sprite *ball;
 
 int main(bool hard)
 {
+	// Load tileset at index 1
 	VDP_loadTileSet(bgtile.tileset, 1, DMA);
+	// Use palette from tileset and assign it to PAL1
 	PAL_setPalette(PAL1, bgtile.palette->data, DMA);
+	// Use tileset index 1 with PAL1 previously defined
 	VDP_fillTileMapRect(BG_B, TILE_ATTR_FULL(PAL1, 0, FALSE, FALSE, 1), 0, 0, 40, 30);
 
+	// All the above works
+
+	//////
+	// Init sprite engine
 	SPR_init();
+	// Add sprite using palette PAL1
 	ball = SPR_addSprite(&imgball, 100, 100, TILE_ATTR(PAL1, FALSE, FALSE, FALSE));
 
 	while (TRUE)
 	{
+		// Should draw sprites
 		SPR_update();
 		VDP_waitVSync();
 	}
